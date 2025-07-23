c51857cafa626a093631ab6a974b22b9cd7507c0
Vince Reuter <vince.reuter@gmail.com>
2025-07-23 14:17:11 +0200

    add project name as top-level constant, and set ZARR format version

--- illumifix/__init__.py
+++ illumifix/__init__.py
@@ -1,4 +1,12 @@
 """Project root"""
 
+from pathlib import Path
+from ome_zarr.format import FormatV02
+
 # Make the __version__ attribute of the project available at the top level.
 from ._version import __version__
+
+
+PROJECT_NAME = Path(__file__).resolve().parent.name
+
+ZARR_FORMAT = FormatV02()
--- tests/test_top_level_package.py
+++ tests/test_top_level_package.py
@@ -2,11 +2,22 @@
 
 import pytest
 
+from ome_zarr.format import FormatV02
+
 import illumifix
+from illumifix import PROJECT_NAME, ZARR_FORMAT
 
 
 def test_version_is_available() -> None:
     try:
-        illumifix.__version__
+        illumifix.__version__  # noqa: B018
     except AttributeError:
         pytest.fail("__version__ is not available as an attribute on the package.")
+
+
+def test_project_name_is_illumifix() -> None:
+    assert PROJECT_NAME == "illumifix"
+
+
+def test_zarr_format_is_as_expected() -> None:
+    assert ZARR_FORMAT == FormatV02()
